830d14e952e767d962d78680676b7899d5b4cb30
EvanYY <yang.yang@77hub.com>
2021-11-10 20:04:02 +0800

    feat: 面板 颜色修改

--- detector.js
+++ detector.js
@@ -1,39 +1,3 @@
-// window.addEventListener("message", (e) => {
-//   if (e.source === window && e.data.easyCanvasDetected) {
-//     chrome.runtime.sendMessage(e.data);
-//   }
-// });
-// sub: {
-//     state={
-//         start:[],
-//         stop:[]
-//     },
-//     addEventListener(target,ev){
-//         if(target in this.state) {
-//             this.state[target].push(ev)
-//         }
-//     } ,
-//     removeEventListener(target,ev) {
-//         if(target in this.state && this.state[target].length) {
-//             const idx = this.state[target].findIndex(v => v === ev)
-//             if(idx !== -1) {
-//                 this.state[target].splice(idx,1)
-//             }
-//         }
-//     },
-//     dispatchEvent(target, res){
-//         if(target in this.state) {
-//             const data = JSON.parse(JSON.stringify(res))
-//             this.state[target].forEach(item => item(data))
-//         }
-//     },
-//     clear(){
-//         this.state = {
-//             start:[],
-//             stop:[]
-//         }
-//     }
-// },
 function detect() {
   window.__EAC_DEV_TOOLS__ = {
     getStatic: function () {
